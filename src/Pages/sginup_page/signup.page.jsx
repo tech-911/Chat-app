190da9b19439f6c91da3f4594306e9e08f25d2d5
tech-911 <babatundeolatunji74@gmail.com>
2021-09-02 09:34:49 +0100

    added background

---
+++ src/Pages/sginup_page/signup.page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./signup.page.scss"
+import Background from "../../assets/Sign-up.png"
+
+function signUp() {
+    return (
+        <div className="signUp-container">
+            
+        </div>
+    )
+}
+
+export default signUp
