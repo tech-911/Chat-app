2bfecc2adc60aa30426a4eb51c4c2f622834a91e
tech-911 <babatundeolatunji74@gmail.com>
2021-09-02 09:49:17 +0100

    trying to fix node sass error

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
+import Signup from "./Pages/signup_page/signup.page";
 
 function App() {
   return (
     <div>
-      
+      <Signup />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Pages/signup_page/signup.page.jsx
+++ src/Pages/signup_page/signup.page.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import "./signup.page.scss"
-import Background from "../../assets/Sign-up.png"
 
 function signUp() {
     return (
         <div className="signUp-container">
-            
+            hello world
         </div>
     )
 }
